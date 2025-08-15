ad54baae988c16b8fb2e39ecb96c0f46a1518a1a
mohamed atia <mohamedd.attia11@gmail.com>
2025-08-16 02:27:04 +0300

    initial configurations

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,9 @@
+export default function MainLayout() {
+  return (
+    <div className="flex flex-col min-h-screen bg-red-600">
+      <main className="grow container mx-auto p-4 bg-amber-500">
+        
+      </main>
+    </div>
+  )
+}
